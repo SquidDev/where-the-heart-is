f74ef45cc5e31617f7060b00304efa15ba38b074
Jonathan Coates <jonathan.coates@freeagent.com>
2018-08-03 16:56:29 +0100

    Make our fish shell look really fancy
     - Sync the colours to match muse-theme.zsh and and zeromini-theme.el
     - Reformat the config file to make it a bit easier to read
     - Define a #RGB -> RGB converter function, to make the file play well
       with rainbow-mode.el.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,62 +1,74 @@
+# We define a function which converts #RGB -> RGB.
+#
+# This allows us to use rainbow-mode when editing colours. While set_colour does
+# appear to accept the leading `#`, I'm not sure I want to rely on that
+# behaviour.
+function _hex
+  string replace -r "^#" "" $argv
+end
+
 # General fish configuration
-set fish_greeting ""
+set fish_greeting     ""
 set fish_key_bindings "fish_default_key_bindings"
 
-# Colours
-set fish_color_autosuggestion "3BA3D0"
-set fish_color_cancel "-r"
-set fish_color_command "0772A1"
-set fish_color_comment "FFE100"
-set fish_color_cwd "87d7ff"
-set fish_color_cwd_root "red"
-set fish_color_dot "af0000"
-set fish_color_end "8D003B"
-set fish_color_error "EC3B86"
-set fish_color_escape "bryellow\x1e--bold"
-set fish_color_history_current "--bold"
-set fish_color_host "normal"
-set fish_color_match "--background\x3dbrblue"
-set fish_color_normal "normal"
-set fish_color_operator "bryellow"
-set fish_color_param "225E79"
-set fish_color_prompt "5fd75f"
-set fish_color_prompt_error "d7005f"
-set fish_color_quote "024A68"
-set fish_color_redirection "63AFD0"
-set fish_color_search_match "bryellow\x1e--background\x3dbrblack"
-set fish_color_selection "white\x1e--bold\x1e--background\x3dbrblack"
-set fish_color_status "red"
-set fish_color_user "brgreen"
-set fish_color_valid_path "--underline"
-set fish_pager_color_completion "\x1d"
-set fish_pager_color_description "B3A06D\x1eyellow"
-set fish_pager_color_prefix "white\x1e--bold\x1e--underline"
-set fish_pager_color_progress "brwhite\x1e--background=cyan"
+# Prompt colours
+set fish_color_cwd               (_hex "#87d7ff")
+set fish_color_dot               (_hex "#af0000")
+set fish_color_prompt            (_hex "#5fd75f")
+set fish_color_prompt_error      (_hex "#d7005f")
+
+# Syntax colours
+set fish_color_normal            "normal"
+set fish_color_command           (_hex "#ccd4e3")
+set fish_color_autosuggestion    (_hex "#61afef")
+set fish_color_param             "normal"
+set fish_color_comment           (_hex "#787080")
+
+set fish_color_cancel            -r
+set fish_color_error             (_hex "#EC3B86")
+set fish_color_history_current   --bold
+set fish_color_match             --background=(_hex "#61afef")
+
+set fish_color_quote             (_hex "#98be65")
+set fish_color_escape            (_hex "#ddbd78")
+set fish_color_valid_path        --underline
+
+set fish_color_end               (_hex "#da8548") # ; and |
+set fish_color_redirection       (_hex "#da8548") # > and <
+set fish_color_operator          (_hex "#da8548") # * and ~
+
+# set fish_color_search_match      "bryellow" "--background=brblack"
+set fish_color_selection         (_hex "#5a5a5a")
+
+set fish_pager_color_prefix      --bold --underline
+set fish_pager_color_completion  ""
+set fish_pager_color_description (_hex "#ddbd78")
+set fish_pager_color_progress    --background=(_hex "#38394c") (_hex "#61afef")
 
 # General git configuation
 set __fish_git_prompt_show_informative_status 1
-set __fish_git_prompt_showuntrackedfiles 0
-set __fish_git_prompt_showupstream informative
+set __fish_git_prompt_showuntrackedfiles      0
+set __fish_git_prompt_showupstream            informative
 
 # Git characters
-set __fish_git_prompt_char_cleanstate "✔"
-set __fish_git_prompt_char_conflictedstate "✖"
-set __fish_git_prompt_char_dirtystate "✹"
-set __fish_git_prompt_char_stagedstate "✚"
+set __fish_git_prompt_char_cleanstate         "✔"
+set __fish_git_prompt_char_conflictedstate    "✖"
+set __fish_git_prompt_char_dirtystate         "✹"
+set __fish_git_prompt_char_stagedstate        "✚"
 
-set __fish_git_prompt_char_stateseparator " "
+set __fish_git_prompt_char_stateseparator     " "
 
-set __fish_git_prompt_char_untrackedfiles "✭"
-set __fish_git_prompt_char_upstream_ahead "↑"
-set __fish_git_prompt_char_upstream_behind "↓"
-set __fish_git_prompt_char_upstream_prefix ""
+set __fish_git_prompt_char_untrackedfiles     "✭"
+set __fish_git_prompt_char_upstream_ahead     "↑"
+set __fish_git_prompt_char_upstream_behind    "↓"
+set __fish_git_prompt_char_upstream_prefix    ""
 
 # Git colours
-set __fish_git_prompt_color_cleanstate "87ff00"
-set __fish_git_prompt_color_dirtystate "d75f00"
-set __fish_git_prompt_color_invalidstate "d70000"
-set __fish_git_prompt_color_stagedstate "5fff00"
+set __fish_git_prompt_color_cleanstate        (_hex "#87ff00")
+set __fish_git_prompt_color_dirtystate        (_hex "#d75f00")
+set __fish_git_prompt_color_invalidstate      (_hex "#d70000")
+set __fish_git_prompt_color_stagedstate       (_hex "#5fff00")
 
-set __fish_git_prompt_color_branch "61afef"
-set __fish_git_prompt_color_untrackedfiles "d7ff00"
-set __fish_git_prompt_color_upstream "ff00d7"
+set __fish_git_prompt_color_branch            (_hex "#61afef")
+set __fish_git_prompt_color_untrackedfiles    (_hex "#d7ff00")
+set __fish_git_prompt_color_upstream          (_hex "#ff00d7")
