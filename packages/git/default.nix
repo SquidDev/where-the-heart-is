6946fe1221bd9a39efd953ef0f2a55e681e99388
Jonathan Coates <git@squiddev.cc>
2024-08-07 16:36:45 +0100

    Add alias for listing stashes
    I have too many!

--- packages/git/default.nix
+++ packages/git/default.nix
@@ -20,6 +20,7 @@
       plot = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'";
       plota = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       graph = "log --decorate --oneline --graph";
+      stashes = "stash list --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ar)%C(reset) %s'";
     };
 
     includes = [
--- profiles/work.nix
+++ profiles/work.nix
@@ -1,5 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   home.username = "jjc";
+  home.packages = [
+    pkgs.gtkwave
+    pkgs.klayout
+  ];
 
   where-the-heart-is.window-decorations = true;
   where-the-heart-is.system-packages = true;
